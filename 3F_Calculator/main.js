95c843e2c723222041cec39aa5f00d8700a843c3
Liz Wang <erosstan33@gmail.com>
2020-05-12 22:24:59 +0800

    要找出比對是否為兩個連續的運算子

--- 3F_Calculator/main.js
+++ 3F_Calculator/main.js
@@ -1,10 +1,22 @@
-const numbers = [0,1, 2, 3, 4, 5, 6, 7, 8, 9];
-const num_button = document.querySelectorAll('.cal-buttons .btn');
-const calResult = document.querySelector('.cal-result');
+const num_button = document.querySelectorAll('.btn');
+const processing = document.querySelector('.processing');
+const numbers = [1,2,3,4,5,6,7,8,9,0];
+const equals = document.querySelector('.equals');
+
+const calResult = [];
+const calResultContainer = document.querySelector('.cal-result');
 
 function apply(){
-  calResult.value = `${this.value}`
-  console.log(this.value);
+  //value 是最新的輸入(最後一筆)
+  const value = this.value;
+  // array[array.length-1] 是倒數第二筆
+  const second = calResult[calResult.length-1];
+  if(second !== numbers){
+    calResult.splice(second);
+  }
+  calResult.push(value);
+  processing.innerHTML = calResult.join("");
+  console.log(calResult);
 }
 
 num_button.forEach( (item) => {
