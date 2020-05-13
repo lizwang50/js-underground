b03b1e3ee1a2d5c9e327c8cb394308b52fcb9cf0
Liz Wang <erossatan33@gmail.com>
2020-05-13 18:57:17 +0800

    update

--- 3F_Calculator/main.js
+++ 3F_Calculator/main.js
@@ -16,9 +16,10 @@ function apply(){
   const calResultLast = calResultArr[calResultArrLen - 1];
   //拿到新陣列的倒數第二筆資料
   const calResultSec = calResultArr[calResultArrLen - 2];
+  
   //比較兩者資料字串中是否包含運算子（ 因為運算子是一群，所以多的包含少的）
-  const compareLast = operators.includes(calResultLast);
-  const compareSec = operators.includes(calResultSec);
+  const compareLast = operators.includes(calResultLast); //布林值
+  const compareSec = operators.includes(calResultSec);   //布林值
   //把最新一筆輸入的東西，推進去陣列裡，但是要擋掉 = 和 AC 這兩個按鈕
   if(value !== '=' && value !== 'AC' && value !== 'Del'){
     calResultArr.push(value);
@@ -27,10 +28,12 @@ function apply(){
   }
   //拿到新陣列的第一筆資料
   const calResultFirst = calResultArr[0];
-  if (calResultFirst == '0') {
+  //比較第一個陣列資料是否為運算子
+  const compareFirst = operators.includes(calResultFirst);   //布林值
+  if (calResultFirst == '0' || compareFirst == true) {
     calResultArr.splice(0,1);
+    return 
   }
-  
   //如果倒數二筆資料等於運算子，從原陣列刪除倒數第二筆資料，顯示在畫面上
   //只留最新的運算子
   if (compareSec == compareLast && compareSec == true) {
@@ -42,31 +45,33 @@ function apply(){
   }
 }
 
-function calculate() {
+function calculate() { 
   //拿到結果字串
-  const result = calResultArr.join("");
-  //排除空字串，避免空值送出
+  const result = calResultArr.join("");  
+  //排除空字串，避免空值送出
   if(result == ''){return}
-  console.log(result);
   //用 eval 將字串轉為程式進行運算
   const total = eval(calResultArr.join(""));
-  console.log(eval(result));
   //將結果呈現在網頁上的 input 值裡面
   calResultContainer.value = total;
+  //清空陣列，準備下一次運算
+  calResultArr = [];
 }
 
 function clear() {
-  console.log(calResultArr);
-  processing.innerHTML = '0';
   //清空字串
-  calResultArr = [];
+  processing.innerHTML = '0';
   //清空陣列
-  calResultContainer.value = '0';
+  calResultArr = [];
   //將結果歸零  
+  calResultContainer.value = '0';
 }
 function deletedNumber() {
+  //pop() 方法會移除並回傳陣列的最後一個元素。此方法會改變陣列的長度。
   calResultArr.pop();
+  //顯示過程
   processing.innerHTML = calResultArr.join("");
+  //如果是空字串，就塞 '0' 進去
   if (processing.innerHTML === '') {
     processing.innerHTML = '0'
   }
