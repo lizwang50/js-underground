e0271391ae3e188a707ebef3f29eae31927875e4
Liz Wang <erossatan33@gmail.com>
2020-05-15 21:23:30 +0800

    update 4f

--- 4F_TimeZone/main.js
+++ 4F_TimeZone/main.js
@@ -11,56 +11,56 @@ const sydneyTime = document.querySelector('.sydney-time');
 
 let event = new Date();
 
-
 function setTaipeiDateTime() {
   let year = event.toLocaleString('en-US', {timeZone:'Asia/Taipei' , year: 'numeric'});
   let month = event.toLocaleString('en-US', {timeZone:'Asia/Taipei' , month: 'short'});
   let day = event.toLocaleString('en-US', {timeZone:'Asia/Taipei' , day: 'numeric'});
-  let time = event.toLocaleTimeString('en-US', {timeZone:'Asia/Taipei',hour12: false,hour: '2-digit', minute: '2-digit'});
+  let time = event.toLocaleTimeString('en-US', {timeZone:'Asia/Taipei',hour12: false});
   taipeiDate.innerHTML = `${day} ${month} . ${year}`;
   taipeiTime.innerHTML = `${time}`;  
 }
-function setNewYorkTime() {
+function setNewYorkDateTime() {
   let year = event.toLocaleString('en-US', {timeZone:'America/New_York' , year: 'numeric'});
   let month = event.toLocaleString('en-US', {timeZone:'America/New_York' , month: 'short'});
   let day = event.toLocaleString('en-US', {timeZone:'America/New_York' , day: 'numeric'});
-  let time = event.toLocaleTimeString('en-US', {timeZone:'America/New_York',hour12: false,hour: '2-digit', minute: '2-digit'});
+  let time = event.toLocaleTimeString('en-US', {timeZone:'America/New_York',hour12: false});
   newYorkDate.innerHTML = `${day} ${month} . ${year}`;
   newYorkTime.innerHTML = `${time}`;  
 }
 
-function setLondonTime() {
+function setLondonDateTime() {
   let year = event.toLocaleString('en-US', {timeZone:'Europe/London' , year: 'numeric'});
   let month = event.toLocaleString('en-US', {timeZone:'Europe/London' , month: 'short'});
   let day = event.toLocaleString('en-US', {timeZone:'Europe/London' , day: 'numeric'});
-  let time = event.toLocaleTimeString('en-US', {timeZone:'Europe/London',hour12: false,hour: '2-digit', minute: '2-digit'});
+  let time = event.toLocaleTimeString('en-US', {timeZone:'Europe/London',hour12: false});
   londonDate.innerHTML = `${day} ${month} . ${year}`;
   londonTime.innerHTML = `${time}`;  
 }
-function setBangkokTime() {
+function setBangkokDateTime() {
   let year = event.toLocaleString('en-US', {timeZone:'Asia/Bangkok' , year: 'numeric'});
   let month = event.toLocaleString('en-US', {timeZone:'Asia/Bangkok' , month: 'short'});
   let day = event.toLocaleString('en-US', {timeZone:'Asia/Bangkok' , day: 'numeric'});
-  let time = event.toLocaleTimeString('en-US', {timeZone:'Asia/Bangkok',hour12: false,hour: '2-digit', minute: '2-digit'});
+  let time = event.toLocaleTimeString('en-US', {timeZone:'Asia/Bangkok',hour12: false});
   bangkokDate.innerHTML = `${day} ${month} . ${year}`;
   bangkokTime.innerHTML = `${time}`;  
 }
-function setSydneyTime() {
+function setSydneyDateTime() {
   let year = event.toLocaleString('en-US', {timeZone:'Australia/Sydney' , year: 'numeric'});
   let month = event.toLocaleString('en-US', {timeZone:'Australia/Sydney' , month: 'short'});
   let day = event.toLocaleString('en-US', {timeZone:'Australia/Sydney' , day: 'numeric'});
-  let time = event.toLocaleTimeString('en-US', {timeZone:'Australia/Sydney',hour12: false,hour: '2-digit', minute: '2-digit'});
+  let time = event.toLocaleTimeString('en-US', {timeZone:'Australia/Sydney',hour12: false});
   sydneyDate.innerHTML = `${day} ${month} . ${year}`;
   sydneyTime.innerHTML = `${time}`;  
 }
 
 
 function updateView() {
+  event = new Date();
   setTaipeiDateTime();
-  setNewYorkTime();
-  setLondonTime();
-  setBangkokTime();
-  setSydneyTime()
+  setNewYorkDateTime();
+  setLondonDateTime();
+  setBangkokDateTime();
+  setSydneyDateTime()
 }
 
-updateView();
+setInterval(updateView,10);
