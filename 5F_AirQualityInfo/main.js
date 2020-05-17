7c8a93fbe7b78d7c5b5a7d92d84643db7e2dfbdb
Liz Wang <erosstan33@gmail.com>
2020-05-17 23:01:36 +0800

    add 5F

--- 5F_AirQualityInfo/main.js
+++ 5F_AirQualityInfo/main.js
@@ -1,11 +1,9 @@
-const xhr = new XMLHttpRequest();
-xhr.open('get','http://opendata.epa.gov.tw/webapi/Data/ATM00679/?$orderby=MonitorDate%20desc&$skip=0&$top=1000&format=json',true);
-xhr.send(null)
+fetch('https://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=1000&%24format=json')
+  .then(function(response) {
+    return response.json();
+  })
+  .then(function(myJson) {
+    console.log(myJson);
+  });
 
-function callOtherDomain() {
-  if(xhr) {    
-    xhr.open('GET', 'http://opendata.epa.gov.tw/webapi/Data/ATM00679/?$orderby=MonitorDate%20desc&$skip=0&$top=1000&format=json', true);
-    xhr.onreadystatechange = handler;
-    xhr.send(); 
-  }
-}
+
