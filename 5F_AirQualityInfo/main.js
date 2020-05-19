d021b0095f513c1acce7bdd6657dbb686dd84a4d
Liz Wang <erossatan33@gmail.com>
2020-05-19 14:42:10 +0800

    completed 5f

--- 5F_AirQualityInfo/main.js
+++ 5F_AirQualityInfo/main.js
@@ -1,31 +1,31 @@
 let data
-const countyOpts = document.querySelector('#searchCity');
+const searchCity = document.querySelector('#searchCity');
 const searchSite = document.querySelector('#searchSite');
-const result = document.querySelector('.result');
+const search = document.querySelector('.search');
+let result = document.querySelectorAll('.result');
 
-const city = document.querySelector('.city');
-const siteView = document.querySelector('.site');
-const updateTime = document.querySelector('.updateTime');
+// const city = document.querySelector('.city');
+// const siteView = document.querySelector('.site');
+// const updateTime = document.querySelector('.updateTime');
 
-const totalAQI = document.querySelector('.totalAQI');
-const status = document.querySelector('.status');
-const o3 = document.querySelector('.o3');
-const pm10 = document.querySelector('.pm10');
-const pm25 = document.querySelector('.pm25');
-const co = document.querySelector('.co');
-const so2 = document.querySelector('.so2');
-const no2 = document.querySelector('.no2');
-
+// const totalAQI = document.querySelector('.totalAQI');
+// const status = document.querySelector('.status');
+// const o3 = document.querySelector('.o3');
+// const pm10 = document.querySelector('.pm10');
+// const pm25 = document.querySelector('.pm25');
+// const co = document.querySelector('.co');
+// const so2 = document.querySelector('.so2');
+// const no2 = document.querySelector('.no2');
 
 const county = [];
-let site = [];
+// let site = [];
 
 fetch('https://opendata.epa.gov.tw/api/v1/AQI?%24skip=0&%24top=1000&%24format=json')
 .then(function(response) {
   return response.json();
 })
 .then(function(json) {
-  console.log('ok');
+  console.log('data ok');
   data = json
   getCounty();
   updateCountySelect();
@@ -41,41 +41,42 @@ function getCounty() {
 }
 
 function filterCountySite(event) {
-  let tempItem = event.target.value;  //是所選的縣市資料
-  let tempArr = [];
-  // 如果 tempItem == data 陣列中的 County 資料，就撈出他的 site 名稱。
+  let tempCounty = event.target.value;  //是所選的縣市資料
   searchSite.innerHTML = '';  
   for (let i = 0; i < data.length; i++) {
-    if (tempItem == data[i].County) {    
+    if (tempCounty == data[i].County) {    
       searchSite.innerHTML += `<option value="${data[i].SiteName}">${data[i].SiteName}</option>`;
     }
   }
-  site = tempArr
-  console.log(tempArr);
-  
 }
 
 function updateCountySelect() {
   county.forEach( i => {
-    countyOpts.innerHTML += `<option value="${i}">${i}</option>`;
+    searchCity.innerHTML += `<option value="${i}">${i}</option>`;
   });
 }
 
 function getResult(e) {
   e.preventDefault();
-  console.log(data.SiteName);
-  if (site == data.SiteName) {
-    totalAQI.textContent = `${data.AQI}`;
-    status.textContent = data.Status;
-    o3.textContent = data.O3;
-    pm10.textContent = data.PM10;
-    pm25.textContent = data.PM10;
-    co.textContent = data.CO;
-    so2.textContent = data.SO2;
-    no2.textContent = data.NO2;
+  const resultSite = searchSite.value;
+  for (let i = 0; i < data.length; i++) {
+    if (resultSite === data[i].SiteName) {
+      console.log(resultSite);
+      result[0].textContent = data[i].County
+      result[1].textContent = data[i].SiteName
+      result[2].textContent = data[i].PublishTime
+      result[3].textContent = data[i].AQI
+      result[4].textContent = data[i].Status
+      result[5].textContent = data[i].O3
+      result[6].textContent = data[i].PM10
+      result[7].textContent = data[i].PM10
+      result[8].textContent = data[i].CO
+      result[9].textContent = data[i].SO2
+      result[10].textContent = data[i].NO2
+    }
+    
   }
-  console.log(site);
 }
 
-countyOpts.addEventListener('change',filterCountySite);
-result.addEventListener('click',getResult)
+searchCity.addEventListener('change',filterCountySite);
+search.addEventListener('click',getResult)
