5a5448bc25f36cbc0174fd2f4bb3a6883234c411
Liz Wang <erossatan33@gmail.com>
2020-05-22 12:15:19 +0800

    helpQQ

--- 6F_CountingGame/main.js
+++ 6F_CountingGame/main.js
@@ -22,7 +22,7 @@ setTimeout(countdownMin, 1000);
 function timer() {
   setTimer = setInterval(countdownSec, 1000);
 }
-
+// Seconds coundown timer
 function countdownSec() {
   // console.log('1')
   seconds--;
@@ -55,44 +55,73 @@ const operator = {
   "÷": "/",
 }
 // default random numbers
-let numberA = Math.ceil(Math.random() * 10);
-let numberB = Math.ceil(Math.random() * 10);
+let numberA = getRandom(1,9);
+let numberB = getRandom(1,9);
 // default random operators obj
 const values = Object.values(operator);
+// operator rules
 let randomOperators = values[parseInt(Math.random() * values.length)];
+// results
 let numbers = [numberA,randomOperators,numberB];
-
 let answer;
 let userAnswerStr;
+divisionQuestion(randomOperators,numberA,numberB);
+getAnswer(numbers);
 
+// get a random number
 function getRandom(min, max){
-  numberA = Math.ceil(Math.random() * (max - min) + min); // 最大到 3 位數
-  numberB = Math.ceil(Math.random() * (max - min) + min);  // 最大到 3 位數
+  return Math.floor(Math.random() * (max - min) + min);
 };
 
+// division
+function divisionQuestion(d,a,b) {
+  if (d == '/') {
+    console.log('除法！');
+    console.log(a,d,b);
+    // numberA / numberB = answer
+    // numberB * answer = numberA
+    // answer = 100/numberB ~ 999/numberB
+    // answer 最小值是比 10/numberB 大的整數
+    // answer 最大值是比 999/numberB 小的整數
+    answer = getRandom(Math.ceil(100/b), Math.floor(999/b));
+    return answer
+  }else{
+    return randomQuestions();
+  }
+}
+
+// start random questions
 function randomQuestions() {
   let currentSec = timerSec.textContent;
   randomOperators = values[parseInt(Math.random() * values.length)];
-  updateQuestions(numberA,numberB);
   if (40 <= currentSec && currentSec < 60 ) {
-    getRandom(1,9);     
+    // 最大到 1 位數
+    numberA = getRandom(1,9); 
+    numberB = getRandom(1,9);
+    console.log('40~60秒',numberA,numberB,answer);
   } else if (20 < currentSec && currentSec <= 39) {
-    getRandom(10,99);   
+    // 最大到 2 位數
+    numberA = getRandom(10,99);
+    numberB = getRandom(10,99);
+    console.log('20~39秒',numberA,numberB,answer);
   } else if (0 < currentSec && currentSec <= 19) {
-    getRandom(100,999); 
+    // A 最大到 3 位數 ; B 最大 2 位數
+    numberA = getRandom(100,999); 
+    numberB = getRandom(10,99);
+    console.log('0~19秒',numberA,numberB,answer);
   }
-  console.log(numbers);
-  
+  divisionQuestion(randomOperators,numberA,numberB);
+  updateQuestions(numberA,numberB);
   getAnswer(numbers);
 }
 
 function getAnswer(num) {
-  answer  = num.join("")
-  answer2 = eval(num.join(""));
-  answer3 = Math.ceil(eval(num.join("")));
-  console.log(answer);
+  answer  = Math.floor(eval(num.join("")));
+  answer2 = num.join("");
+  console.log(num,'=',answer);
 }
 
+// get users answers and random next questions
 function getUsersAnswers() {
   userAnswerInput.addEventListener("keyup", function (event) {
     if (userAnswerInput.value !== '' && event.key === "Enter") {
