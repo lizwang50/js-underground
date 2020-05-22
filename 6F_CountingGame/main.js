2a347f0ece0840e8565280974448aa01fa7d2daf
Liz Wang <erossatan33@gmail.com>
2020-05-22 18:03:10 +0800

    backup

--- 6F_CountingGame/main.js
+++ 6F_CountingGame/main.js
@@ -49,14 +49,14 @@ timer();
 const questionView = document.querySelector('.question-view');
 const userAnswerInput = document.querySelector('.answer');
 const operator = {
-  " ": "+",
+  "+": "+",
   "-": "-",
   "x": "*",
   "÷": "/",
 }
 // default random numbers
-let numberA = getRandom(1,9);
 let numberB = getRandom(1,9);
+let numberA = getRandom(1,9);
 // default random operators obj
 const values = Object.values(operator);
 // operator rules
@@ -65,78 +65,77 @@ let randomOperators = values[parseInt(Math.random() * values.length)];
 let numbers = [numberA,randomOperators,numberB];
 let answer;
 let userAnswerStr;
-divisionQuestion(randomOperators,numberA,numberB);
-getAnswer(numbers);
 
 // get a random number
 function getRandom(min, max){
+  console.log('getRandom', min,max)
   return Math.floor(Math.random() * (max - min) + min);
 };
 
-// division
-function divisionQuestion(d,a,b) {
-  if (d == '/') {
-    console.log('除法！');
-    console.log(a,d,b);
-    // numberA / numberB = answer
-    // numberB * answer = numberA
-    // answer = 100/numberB ~ 999/numberB
-    // answer 最小值是比 10/numberB 大的整數
-    // answer 最大值是比 999/numberB 小的整數
-    answer = getRandom(Math.ceil(100/b), Math.floor(999/b));
-    return answer
-  }else{
-    return randomQuestions();
-  }
+function createNumbers(o,a,b) {
+  console.log('createNumbers', o,a,b)
+    // 如果是除法，而且 a / b 有餘數，不可以回傳
+    if (o == '/' && a % b !== 0){
+      console.log('不可以回傳的除法');
+      randomQuestions();
+    }else{
+      updateQuestionsView();
+      getAnswer(numbers);
+    }
 }
 
 // start random questions
 function randomQuestions() {
+  console.log('randomQuestions')
   let currentSec = timerSec.textContent;
   randomOperators = values[parseInt(Math.random() * values.length)];
   if (40 <= currentSec && currentSec < 60 ) {
     // 最大到 1 位數
     numberA = getRandom(1,9); 
     numberB = getRandom(1,9);
-    console.log('40~60秒',numberA,numberB,answer);
+    // console.log('40~60秒',numberA,numberB);
   } else if (20 < currentSec && currentSec <= 39) {
     // 最大到 2 位數
     numberA = getRandom(10,99);
     numberB = getRandom(10,99);
-    console.log('20~39秒',numberA,numberB,answer);
+    // console.log('20~39秒',numberA,numberB);
   } else if (0 < currentSec && currentSec <= 19) {
     // A 最大到 3 位數 ; B 最大 2 位數
     numberA = getRandom(100,999); 
     numberB = getRandom(10,99);
-    console.log('0~19秒',numberA,numberB,answer);
-  }
-  divisionQuestion(randomOperators,numberA,numberB);
-  updateQuestions(numberA,numberB);
-  getAnswer(numbers);
+    // console.log('0~19秒',numberA,numberB);
+  }else { console.log("QQ")}
+  createNumbers(randomOperators,numberA,numberB);
 }
 
 function getAnswer(num) {
+  console.log('getAnswer',num)
   answer  = Math.floor(eval(num.join("")));
-  answer2 = num.join("");
-  console.log(num,'=',answer);
+  // console.log(num,'=',answer);
 }
 
 // get users answers and random next questions
 function getUsersAnswers() {
+  console.log('getUsersAnswers')
   userAnswerInput.addEventListener("keyup", function (event) {
     if (userAnswerInput.value !== '' && event.key === "Enter") {
       randomQuestions();
       userAnswerStr = userAnswerInput.value;
+      console.log(userAnswerStr,'is user typing');
       return userAnswerInput.value = '';
     }
   });
 }
 
-function updateQuestions() {
+function updateQuestionsView() {
+  // console.log('updateQuestionsView');
   questionView.textContent = `${numberA} ${randomOperators} ${numberB}`;
   getUsersAnswers();
+  // console.log(randomOperators);
 }
 
-updateQuestions();
-
-
+// get question and answer
+// getAnswer(numbers);
+randomQuestions();
+createNumbers(randomOperators,numberA,numberB);
+// console.log(numbers,'=',answer,'default');
