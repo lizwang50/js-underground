c78ed3a14a27892e3f660e80df18bd1e372cbddc
Liz Wang <erossatan33@gmail.com>
2020-06-03 17:42:08 +0800

    QQ

--- 8F_TicTacToe/main.js
+++ 8F_TicTacToe/main.js
@@ -9,32 +9,37 @@ const playerA = document.querySelector('.player-A');
 const playerB = document.querySelector('.player-B');
 
 let isPlay = false;
-let recordArray = [];
+let ArrayA = [];
+let ArrayB = [];
 
 playground.forEach((item) => {
-  item.addEventListener('click',startGameRecord);
+	item.addEventListener('click',startGameRecord);
 })
 
 function startGameRecord() {
-  if (this.innerHTML !== '') return
-  let dataObj ={
+	if (this.innerHTML !== '') return
+	console.log('startGameRecord');
+  let dataObj = {
     placeId: this.dataset.id,
     textContent: 'YOUR TURN!'
-  };
+	};
   isPlay = !isPlay;
   if(isPlay){
-    dataObj.player = 'A'
-    dataObj.className ='circle'
-    }else{
-    dataObj.player = 'B'
-    dataObj.className = 'cross'
-  }
-  applyMark(recordArray,dataObj,this);
+		dataObj.player = 'A'
+		dataObj.className ='circle'
+		ArrayA.push(dataObj.placeId)
+	}else{
+		dataObj.player = 'B'
+		dataObj.className = 'cross'
+		ArrayB.push(dataObj.placeId)
+	}
+	compareResult(ArrayA,ArrayB);
+	applyMark(dataObj,this);
 }
-function applyMark(array,obj,target) {
-  array.push(obj);
-  target.innerHTML = `<div class="${obj.className}"></div>`;
-  // 5. Your Turn 功能 
+
+function applyMark(obj,target) {
+	target.innerHTML = `<div class="${obj.className}"></div>`;
+  // 5. Your Turn 功能
   if (obj.player == 'A') {
     playerA.textContent = obj.textContent
     playerB.textContent = ''
@@ -51,12 +56,33 @@ function restartGame() {
   console.log('restartGame');
   isPlay = false;
   playerA.textContent = ''
-  playerB.textContent = ''
+	playerB.textContent = ''
+	ArrayA = [];
+	ArrayB = [];
   playground.forEach((i)=>{i.innerHTML = '';})
 }
 // 3. 規則：
 // 3.1 三個 O 連成一條線贏
 // 3.2 三個 X 連成一條線贏
 // 3.3 沒有連成一條線就是平手
+const winRule = {
+	0: ['1','4','7'],
+	1: ['2','5','8'],
+	2: ['3','6','9'],
+	3: ['1','2','3'],
+	4: ['4','5','6'],
+	5: ['7','8','9'],
+	6: ['1','5','9'],
+	7: ['3','5','7'],
+}
 
+function compareResult(a,b) {
+	if(a.length < 3 && b.length < 3) return
+	console.log('compareResult');
+	for (let i = 0; i < 8; i++) {
+			console.log('array A',a);
+			console.log(winRule[i].includes(a));
+			console.log('winRule',winRule[i]);
+	}
+}
 // 4. 記錄戰績功能（Local Storage）
